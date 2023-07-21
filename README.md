311cf7649a0edd8ba45ccc82ea6a68e6af2cdb38
juanbozuna <59804620+sergiovegam41@users.noreply.github.com>
2023-07-20 23:41:59 -0500

    fix: environment

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 
 ### INSTALACION DEL PROYECTO ###
 
-#1 Crear una base de datos llamada "smart_info"
-#2 Importar el archivo smart_info_datebase.sql que se encuentra en el directorio 'smart_info_datebase.sql'
-#3 acceder al proyecto
+#1 configurar coneccion con base de datos en ".\lib\.env"
+#2 Importar el archivo smart_info_datebase.sql que se encuentra en el directorio '.\smart_info_datebase.sql'
+#3 composer install y acceder al proyecto
--- lib/config.php
+++ lib/config.php
@@ -1,7 +1,9 @@
 <?php
-
 date_default_timezone_set('America/Bogota');
 
+require_once __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__."\..");
+$dotenv->load();
 
 require_once  __DIR__."/php-activerecord/ActiveRecord.php";
 
@@ -11,9 +13,9 @@ ActiveRecord\Config::initialize(function($cfg)
   //  $cfg->set_model_directory(__DIR__.'/../models/Essays');
    $cfg->set_connections(
      array(
-       'development' => 'mysql://root:@localhost:3306/smart_info',
-       'test' => 'mysql://root:@localhost:3306/smart_info',
-       'production' => 'mysql://root:@localhost:3306/smart_info'
+       'development' => 'mysql://'.$_ENV['DATABASE_USER'].':'.$_ENV['DATABASE_PASSWORD'].'@'.$_ENV['DATABASE_HOST'].':'.$_ENV['DATABASE_PORT'].'/'.$_ENV['DATABASE_NAME'],
+       'test' => 'mysql://'.$_ENV['DATABASE_USER'].':'.$_ENV['DATABASE_PASSWORD'].'@'.$_ENV['DATABASE_HOST'].':'.$_ENV['DATABASE_PORT'].'/'.$_ENV['DATABASE_NAME'],
+       'production' => 'mysql://'.$_ENV['DATABASE_USER'].':'.$_ENV['DATABASE_PASSWORD'].'@'.$_ENV['DATABASE_HOST'].':'.$_ENV['DATABASE_PORT'].'/'.$_ENV['DATABASE_NAME']
      )
    );
 });
